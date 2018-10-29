3dfef04a53b35d9543cc2cd31de2a6d8f4bf9cc7
Zhibin Yu <zhibinyu97@gmail.com>
2018-10-29 16:19:26 +0800

    Refactor folder structure

--- utils/config.js
+++ utils/config.js
@@ -1,14 +1,13 @@
-var config = {};
+let config = {};
 
 config.db = {};
-config.server = {};
-config.secret = {};
-
 config.db.addr = 'mongodb://localhost/toiletGoWhere';
 
+config.server = {};
 config.server.http_port = 3000;
 config.server.route = './routes/routes';
 
+config.secret = {};
 config.secret.jwt = 'A_Random_Secret_String';
 
 module.exports = config;
