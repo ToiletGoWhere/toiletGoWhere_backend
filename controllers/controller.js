258204af2c1532c42ad53af7647eea73dd1528a2
Zhibin Yu <zhibinyu97@gmail.com>
2018-11-06 15:30:06 +0800

    Remove the sample controller

--- controllers/controller.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-exports.ping = (req, res) => {
-    console.log(
-        `${new Date().toLocaleString()}|A request for ${req.originalUrl}`,
-    );
-    res.status(200).send("Server is running properly...");
-};
