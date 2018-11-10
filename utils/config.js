72a15f101a3e07ff766ba6f4b4370bd314f6b8a7
Zhibin Yu <zhibinyu97@gmail.com>
2018-11-10 11:53:33 +0800

    Update config file

--- utils/config.js
+++ utils/config.js
@@ -7,8 +7,4 @@ config.server = {};
 config.server.http_port = 3000;
 config.server.route = "./routes/routes";
 
-config.secret = {};
-config.secret.jwt = "A_Random_Secret_String";
-config.secret.googleMapKey = "AIzaSyAQ2vkTBfGCJSae3KqB6C0VrbsN2TMHuSo";
-
 module.exports = config;
